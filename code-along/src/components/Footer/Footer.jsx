aeac8cc4ba61ca8a9e3c196193d7e5488e66be9c
Daniela <daniela.fedyakin@gmail.com>
2021-08-15 21:29:58 +0200

    extend footer

--- code-along/src/components/Footer/Footer.jsx
+++ code-along/src/components/Footer/Footer.jsx
@@ -1,24 +1,56 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+
+import links from '../../data/footerLinks.json';
+
 import BrandLink from '../BrandLink';
+import FacebookIcon from '../Icons/FacebookIcon';
+import TwitterIcon from '../Icons/TwitterIcon';
+import InstagramIcon from '../Icons/InstagramIcon';
 
 import './Footer.css';
 
-const Footer = () => {
-    const links = ['about', 'skincare', 'makeup', 'supplements', 'contact'];
+const Footer = () => (
+    <footer className="bg-dark text-light flex justify-between p-8">
+        <BrandLink classes="text-yellow pl-4 inline-block" />
 
-    return (
-        <footer className="bg-dark text-light flex justify-between px-4 py-4">
-            <BrandLink classes="text-yellow" />
+        <div className="flex justify-between mx-auto">
+            {links.map(item => (
+                <div className="flex flex-col flex-wrap mr-10">
+                    <h6 className="font-krona text-sm">{item.title}</h6>
+                    <div>
+                        {item.links.map(link => (
+                            <Link to={link.path} key={link} className="lowercase text-sm block hover:text-yellow">
+                                {link.name}
+                            </Link>
+                        ))}
+                    </div>
+                </div>
+            ))}
+        </div>
 
-            <div className="flex items-center">
-                {links.map(link => (
-                    <a href="/" key={link} className="ml-4">
-                        {link}
-                    </a>
-                ))}
-            </div>
-        </footer>
-    );
-};
+        <div className="text-yellow pr-4">
+            <a
+                href="https://facebook.com"
+                target="_blank"
+                rel="noreferrer"
+                className="hover:text-light-grey block mb-4"
+            >
+                <FacebookIcon />
+            </a>
+            <a href="https://twitter.com" target="_blank" rel="noreferrer" className="hover:text-light-grey block mb-4">
+                <TwitterIcon />
+            </a>
+            <a
+                href="https://instagram.com"
+                target="_blank"
+                rel="noreferrer"
+                className="hover:text-light-grey block mb-4"
+            >
+                <InstagramIcon />
+            </a>
+        </div>
+    </footer>
+);
 
 export default Footer;
---
+++ code-along/src/components/Icons/FacebookIcon.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const FacebookIcon = () => (
+    <svg className="h-6 w-6" viewBox="0 0 10 18" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path
+            d="M6.06595 18V9.80271H8.83139L9.24245 6.59321H6.06595V4.54888C6.06595 3.62274 6.324 2.98864 7.6532 2.98864H9.33747V0.127189C8.51798 0.0393664 7.69426 -0.00303849 6.87008 0.000169193C4.4257 0.000169193 2.74743 1.4924 2.74743 4.23183V6.58721H0V9.79671H2.75343V18H6.06595Z"
+            fill="currentColor"
+        />
+    </svg>
+);
+
+export default FacebookIcon;
